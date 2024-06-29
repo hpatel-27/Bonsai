daa1abd50864a9cfcc4696ebca97c905ad3d54f9
hpatel-27 <harshpxv@gmail.com>
2024-06-28 21:39:17 -0400

    Starting testing for the api, moving to a test branch

--- Bonsai/src/test/java/hpatel/Bonsai/api/APITest.java
+++ Bonsai/src/test/java/hpatel/Bonsai/api/APITest.java
@@ -1,13 +1,10 @@
 package hpatel.Bonsai.api;
 
-import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 
 import javax.transaction.Transactional;
 
@@ -17,11 +14,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import hpatel.Bonsai.models.Workout;
 
 /**
  * Class for testing the API interface
@@ -51,5 +49,27 @@ public class APITest {
         mvc = MockMvcBuilders.webAppContextSetup( context ).build();
     }
 
+    /**
+     * Tests the API
+     *
+     * @throws Exception
+     *             if the API call fails
+     *
+     * @throws UnsupportedEncodingException
+     *             if the encoding fails
+     *
+     */
+    @Test
+    @Transactional
+    public void testAPI () throws Exception {
+        // get any workouts
+        final String workout = mvc.perform( get( "/api/v1/workouts" ) ).andDo( print() ).andExpect( status().isOk() )
+                .andReturn().getResponse().getContentAsString();
+
+        if ( !workout.contains( "Push" ) ) {
+            // create the workout if its not there
+            final Workout w = new Workout();
+        }
+    }
 
 }
