0c142c4b8f2519b65b6572330eee1e71b9b5023a
hpatel-27 <harshpxv@gmail.com>
2024-07-23 22:13:27 -0400

    [Testing] Completing all the initial unit testing for workouts

--- Bonsai/src/test/java/hpatel/Bonsai/unit/WorkoutTest.java
+++ Bonsai/src/test/java/hpatel/Bonsai/unit/WorkoutTest.java
@@ -3,6 +3,7 @@ package hpatel.Bonsai.unit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
@@ -15,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import hpatel.Bonsai.TestConfig;
+import hpatel.Bonsai.models.Exercise;
 import hpatel.Bonsai.models.Workout;
 import hpatel.Bonsai.services.WorkoutService;
 
@@ -45,6 +47,119 @@ public class WorkoutTest {
         assertEquals( "Push", w1.getName() );
         assertEquals( LocalDate.now().toString(), w1.getDate() );
         assertEquals( 0, w1.getExercises().size() );
+
+        List<Workout> workouts = workoutService.findAll();
+
+        assertEquals( 0, workouts.size() );
+
+        workoutService.save( w1 );
+
+        workouts = workoutService.findAll();
+
+        assertEquals( 1, workouts.size() );
+
+        Workout ws1 = workouts.get( 0 );
+
+        assertEquals( "Push", ws1.getName() );
+        assertEquals( LocalDate.now().toString(), ws1.getDate() );
+        assertEquals( 0, ws1.getExercises().size() );
+
+        // now add two exercises to the workout and then save it and check that
+        // the object matches
+        final Exercise ex1 = new Exercise( "Bench Press", "Chest, Triceps, Shoulders", 135.0, 4, 10 );
+        final Exercise ex2 = new Exercise( "Squat", "Quads, Hamstrings, Calves, Core", 245.0, 4, 6 );
+
+        ws1.addExercise( ex1 );
+        ws1.addExercise( ex2 );
+
+        assertEquals( 2, ws1.getExercises().size() );
+
+        workoutService.save( ws1 );
+
+        workouts = workoutService.findAll();
+
+        assertEquals( 1, workouts.size() );
+
+        ws1 = workouts.get( 0 );
+
+        assertEquals( "Push", ws1.getName() );
+        assertEquals( LocalDate.now().toString(), ws1.getDate() );
+        assertEquals( 2, ws1.getExercises().size() );
+
+        List<Exercise> exercises = ws1.getExercises();
+
+        final Exercise e1 = exercises.get( 0 );
+        final Exercise e2 = exercises.get( 1 );
+
+        assertEquals( "Bench Press", e1.getName() );
+        assertEquals( "Chest, Triceps, Shoulders", e1.getDescription() );
+        assertEquals( 135.0, e1.getWeight() );
+        assertEquals( 4, e1.getSets() );
+        assertEquals( 10, e1.getReps() );
+
+        assertEquals( "Squat", e2.getName() );
+        assertEquals( "Quads, Hamstrings, Calves, Core", e2.getDescription() );
+        assertEquals( 245.0, e2.getWeight() );
+        assertEquals( 4, e2.getSets() );
+        assertEquals( 6, e2.getReps() );
+
+        // create an object, save the object, and check that it matches
+        final Workout w2 = new Workout( "Pull" );
+
+        assertEquals( "Pull", w2.getName() );
+        assertEquals( LocalDate.now().toString(), w2.getDate() );
+        assertEquals( 0, w2.getExercises().size() );
+
+        workouts = workoutService.findAll();
+
+        assertEquals( 1, workouts.size() );
+
+        workoutService.save( w2 );
+
+        workouts = workoutService.findAll();
+
+        assertEquals( 2, workouts.size() );
+
+        Workout ws2 = workouts.get( 1 );
+
+        assertEquals( "Pull", ws2.getName() );
+        assertEquals( LocalDate.now().toString(), ws2.getDate() );
+        assertEquals( 0, ws2.getExercises().size() );
+
+        final Exercise ex3 = new Exercise( "Deadlift", "Core", 315.0, 1, 5 );
+        final Exercise ex4 = new Exercise( "Row", "Back, Biceps, Forearms", 185.0, 2, 10 );
+
+        ws2.addExercise( ex3 );
+        ws2.addExercise( ex4 );
+
+        workoutService.save( ws2 );
+
+        workouts = workoutService.findAll();
+
+        assertEquals( 2, workouts.size() );
+
+        ws2 = workouts.get( 1 );
+
+        assertEquals( "Pull", ws2.getName() );
+        assertEquals( LocalDate.now().toString(), ws2.getDate() );
+        assertEquals( 2, ws2.getExercises().size() );
+
+        exercises = ws2.getExercises();
+
+        final Exercise e3 = exercises.get( 0 );
+        final Exercise e4 = exercises.get( 1 );
+
+        assertEquals( "Deadlift", e3.getName() );
+        assertEquals( "Core", e3.getDescription() );
+        assertEquals( 315.0, e3.getWeight() );
+        assertEquals( 1, e3.getSets() );
+        assertEquals( 5, e3.getReps() );
+
+        assertEquals( "Row", e4.getName() );
+        assertEquals( "Back, Biceps, Forearms", e4.getDescription() );
+        assertEquals( 185.0, e4.getWeight() );
+        assertEquals( 2, e4.getSets() );
+        assertEquals( 10, e4.getReps() );
 
     }
 }
