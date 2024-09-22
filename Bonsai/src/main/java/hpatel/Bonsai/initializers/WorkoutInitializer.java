247eb0ce01e5a55e773de3663a483de9fc5bfac4
Harsh <hpatel27@ncsu.edu>
2024-09-22 01:12:52 -0400

    Adding initial workout data was causing and DataIntegrityViolation
    Created a test profile for testing and did not load the initial data for
    testing, this prevents any dangling references.
    
    Also tried to enable orphan removal for this so that maybe i could
    remove a workout and it would also remove the related exercises.
    
    My best guess is that since I was using the same exercise object in
    multiple places they were being used for different workout objects as
    well, leading to a situation where I delete one of the workouts and it
    removes the reference to the Exercise, which is a problem since that
    same Exercise is on another Workout.

--- Bonsai/src/main/java/hpatel/Bonsai/initializers/WorkoutInitializer.java
+++ Bonsai/src/main/java/hpatel/Bonsai/initializers/WorkoutInitializer.java
@@ -4,8 +4,10 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-import hpatel.Bonsai.repositories.ExerciseRepository;
+import hpatel.Bonsai.models.Exercise;
+import hpatel.Bonsai.models.Workout;
 import hpatel.Bonsai.repositories.WorkoutRepository;
 
 /**
@@ -15,49 +17,39 @@ import hpatel.Bonsai.repositories.WorkoutRepository;
  *
  */
 @Configuration
+@Profile ( "!test" ) // Excludes this bean when the 'test' profile is active
 public class WorkoutInitializer {
 
     @Autowired
-    private WorkoutRepository  workoutRepository;
-
-    @Autowired
-    private ExerciseRepository exerciseRepository;
+    private WorkoutRepository workoutRepository;
 
     @PostConstruct
     public void intializeWorkouts () {
-        // // check if there are no existing workouts
-        // if ( workoutRepository.count() == 0 ) {
-        //
-        // // Make a new workout for Back and Biceps
-        // final Workout w1 = new Workout( "Back, Biceps" );
-        //
-        // // Change the date for September 15, 2024
-        // w1.setDate( "2024-09-15" );
-        //
-        // // Make some exercises and save them to the db
-        // final Exercise e1 = new Exercise( "Deadlift", "Core, Back, Biceps,
-        // Legs", 315, 3, 1 );
-        // final Exercise e2 = new Exercise( "Bent-Over Row", "Back, Biceps",
-        // 165, 3, 10 );
-        // final Exercise e3 = new Exercise( "Standard Curls", "Biceps", 30, 3,
-        // 10 );
-        //
-        // exerciseRepository.save( e1 );
-        // exerciseRepository.save( e2 );
-        // exerciseRepository.save( e3 );
-        //
-        // // add the exercises to the workout
-        // w1.addExercise( e1 );
-        // w1.addExercise( e2 );
-        // w1.addExercise( e3 );
-        //
-        // // save the workout
-        // workoutRepository.save( w1 );
-        // System.out.println( "Initial data inserted." );
-        // }
-        // else {
-        // System.out.println( "Data already exists, skipping initialization."
-        // );
-        // }
+        // check if there are no existing workouts
+        if ( workoutRepository.count() == 0 ) {
+
+            // Make a new workout for Back and Biceps
+            final Workout w1 = new Workout( "Back, Biceps" );
+
+            // Change the date for September 15, 2024
+            w1.setDate( "2024-09-15" );
+
+            // Make some exercises and save them to the db
+            final Exercise e1 = new Exercise( "Deadlift", "Core, Back, Biceps, Legs", 315, 3, 1 );
+            final Exercise e2 = new Exercise( "Bent-Over Row", "Back, Biceps", 165, 3, 10 );
+            final Exercise e3 = new Exercise( "Standard Curls", "Biceps", 30, 3, 10 );
+
+            // add the exercises to the workout
+            w1.addExercise( e1 );
+            w1.addExercise( e2 );
+            w1.addExercise( e3 );
+
+            // save the workout
+            workoutRepository.save( w1 );
+            System.out.println( "Initial data inserted." );
+        }
+        else {
+            System.out.println( "Data already exists, skipping initialization." );
+        }
     }
 }
